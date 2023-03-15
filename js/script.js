9d04937cbe26bd7cc86c23d01e0aac6c9d592f87
wonejito <30444850+wonejito@users.noreply.github.com>
2023-03-15 12:02:21 -0400

    I think i found out the logic *again*

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const addBookBtn = document.getElementById("add-book-btn");
 const form = document.getElementById("book-form");
+const main = document.getElementById("main");
+const card = document.getElementsByClassName("book-card");
 const myLibrary = [];
 
 function Book(title, author, pages) {
@@ -8,17 +10,17 @@ function Book(title, author, pages) {
 	this.pages = pages;
 }
 
-function addBookToLibrary() {}
-
-addBookBtn.addEventListener("click", function () {
-	// formVisibility(form);
+form.addEventListener("submit", function (event) {
+	const arrayForm = [...form];
+	const thistitle = arrayForm[0].value;
+	const thisauthor = arrayForm[1].value;
+	const thispages = arrayForm[2].value;
+	function addBookToLibrary() {
+		const book = new Book(thistitle, thisauthor, thispages);
+		myLibrary.push(book);
+	}
 	addBookToLibrary();
+	event.preventDefault();
 });
 
-// function formVisibility(thiselement) {
-// 	if (thiselement.style.display === "none") {
-// 		thiselement.style.display = "inline";
-// 	} else {
-// 		thiselement.style.display = "none";
-// 	}
-// }
+function displayBooks(collection) {}
