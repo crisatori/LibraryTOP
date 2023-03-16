b493741d553c334c3dedd722dd7a04afa7f21b26
wonejito <30444850+wonejito@users.noreply.github.com>
2023-03-16 18:46:58 -0400

    Re-starting... :///

--- js/script.js
+++ js/script.js
@@ -1,49 +1,14 @@
 const addBookBtn = document.querySelector("#add-book-btn");
 const form = document.querySelector("#book-form");
 const main = document.getElementById("main");
+const removeBook = document.querySelector(".remove-book-btn");
 
-const myLibrary = [
-	{
-		title: "The Little Prince",
-		author: "Antoine de Saint-Exupéry",
-		pages: "250",
-	},
-];
+const myLibrary = [];
 
-function Book(title, author, pages) {
-	this.title = title;
-	this.author = author;
-	this.pages = pages;
+function Book() {
+	// the constructor...
 }
 
-function addBookToLibrary(collection) {
-	const title = form.querySelector("#book-title").value;
-	const author = form.querySelector("#book-author").value;
-	const pages = form.querySelector("#book-pages").value;
-	const newBook = new Book(title, author, pages);
-	collection.push(newBook);
+function addBookToLibrary() {
+	// do stuff here
 }
-
-form.addEventListener("submit", function (event) {
-	addBookToLibrary(myLibrary);
-	event.preventDefault();
-	const card = document.querySelector(".book-card");
-	const clone = card.cloneNode(true);
-	clone.querySelector(".title").textContent = myLibrary[myLibrary.length - 1].title;
-	clone.querySelector(".author").textContent = myLibrary[myLibrary.length - 1].author;
-	clone.querySelector(".pages").textContent = myLibrary[myLibrary.length - 1].pages;
-	main.appendChild(clone);
-	DisplayIt(form);
-});
-
-function DisplayIt(element) {
-	if (element.style.display === "none") {
-		element.style.display = "inline";
-	} else {
-		element.style.display = "none";
-	}
-}
-
-addBookBtn.addEventListener("click", function () {
-	DisplayIt(form);
-});
