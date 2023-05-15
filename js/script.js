66ccdb3197220b70524173da5093f9cd0f443903
wonejito <30444850+wonejito@users.noreply.github.com>
2023-05-15 17:29:55 -0400

    Trying to make the function that removes element from the collection and from the display

--- js/script.js
+++ js/script.js
@@ -2,39 +2,54 @@ let bookShelf = document.querySelector(".book-shelf");
 const addBookBtn = document.querySelector("#addBookBtn");
 const submitBtn = document.querySelector("#submit-btn");
 const form = document.querySelector("form");
+let newBook;
 // let newFuckingBook;
-let myLibrary = [
-  { title: "The Book", author: "The Author", pages: "The Pages" },
-];
+let myLibrary = [];
 
-function Book(bTitle, bAuthor, bPages) {
+function Book(bTitle, bAuthor, bPages, id) {
   this.title = bTitle;
   this.author = bAuthor;
   this.pages = bPages;
+  this.id = id;
 }
 
 function displayBooks(collection) {
   bookShelf.innerHTML = "";
   for (let i = 0; i < collection.length; i++) {
-    let newBook = document.createElement("div");
+    newBook = document.createElement("div");
     newBook.className = "book-card";
-    newBook.innerHTML = `<div><h2 class="book-title">Title:</h2><span class="title">${collection[i].title}</span></div><div><h2 class="book-author">Author:</h2><span class="author">${collection[i].author}</span></div><div><h2 class="book-no-pages">No. Pages:</h2><span class="no-pages">${collection[i].pages}</span></div>`;
+    newBook.innerHTML = `<div id="${collection[i].id}"><h2 class="book-title">Title:</h2><span class="title">${collection[i].title}</span></div><div><h2 class="book-author">Author:</h2><span class="author">${collection[i].author}</span></div><div><h2 class="book-no-pages">No. Pages:</h2><span class="no-pages">${collection[i].pages}</span><img src="assests/delete.png" /></div>`;
     bookShelf.appendChild(newBook);
   }
 }
-// displayBooks(myLibrary);
 
 function addBookToLibrary(title, author, pages) {
-  nBookToCollection = new Book(title, author, pages);
+  let id = `book-${myLibrary.length}`;
+  nBookToCollection = new Book(title, author, pages, id);
   myLibrary.push(nBookToCollection);
+  displayBooks(myLibrary);
 }
 
-submitBtn.addEventListener("click", function (e) {
-  const titleForm = form.querySelector("#title-form");
-  const authorForm = form.querySelector("#author-form");
-  const pagesForm = form.querySelector("#pages-form");
-  addBookToLibrary(titleForm.value, authorForm.value, pagesForm.value);
-  form.style.cssText = "display: none";
-  displayBooks(myLibrary);
-  e.preventDefault();
+addBookToLibrary("El Princeso", "Juan", 230);
+addBookToLibrary("Deja el relajo", "Carlos", 230);
+addBookToLibrary("Deja el relajo", "Carlos", 230);
+
+// displayBooks(myLibrary);
+
+// submitBtn.addEventListener("click", function (e) {
+//   const titleForm = form.querySelector("#title-form");
+//   const authorForm = form.querySelector("#author-form");
+//   const pagesForm = form.querySelector("#pages-form");
+//   addBookToLibrary(titleForm.value, authorForm.value, pagesForm.value);
+//   e.preventDefault();
+// });
+let allBooks = bookShelf.querySelectorAll("img");
+allBooks.forEach((book) => {
+  book.addEventListener("click", function () {
+    let parent = this.parentElement.parentElement;
+    let identification = parent.querySelector("div").id;
+    let thisIndex = myLibrary.indexOf(identification);
+    myLibrary.splice(thisIndex, 1);
+    console.log(thisIndex);
+  });
 });
