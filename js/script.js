7e7b422328ea2d72906e7c52d5f18bb8ef1e535f
Cristopher F <30444850+crisatori@users.noreply.github.com>
2023-07-07 11:39:44 -0400

    Made some changes.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
-let bookShelf = document.querySelector(".book-shelf");
+const bookShelf = document.querySelector(".book-shelf");
 const addBookBtn = document.querySelector("#addBookBtn");
 const submitBtn = document.querySelector("#submit-btn");
 const form = document.querySelector("form");
 let newBook;
-// let newFuckingBook;
-let myLibrary = [];
+const myLibrary = [];
 
 function Book(bTitle, bAuthor, bPages, id) {
 	this.title = bTitle;
 	this.author = bAuthor;
 	this.pages = bPages;
 	this.id = id;
+	this.read = false;
+	this.isRead = function () {
+		this.read = !this.read;
+	};
 }
 
 function displayBooks(collection) {
@@ -18,14 +21,14 @@ function displayBooks(collection) {
 	for (let i = 0; i < collection.length; i++) {
 		newBook = document.createElement("div");
 		newBook.className = "book-card";
-		newBook.innerHTML = `<div id="${collection[i].id}"><h2 class="book-title">Title:</h2><span class="title">${collection[i].title}</span></div><div><h2 class="book-author">Author:</h2><span class="author">${collection[i].author}</span></div><div><h2 class="book-no-pages">No. Pages:</h2><span class="no-pages">${collection[i].pages}</span><img src="assests/delete.png" /></div>`;
+		newBook.innerHTML = `<div id="${collection[i].id}"><h2 class="book-title">Title:</h2><span class="title">${collection[i].title}</span></div><div><h2 class="book-author">Author:</h2><span class="author">${collection[i].author}</span></div><div><h2 class="book-no-pages">No. Pages:</h2><span class="no-pages">${collection[i].pages}</span><img src="assests/deconste.png" /></div>`;
 		bookShelf.appendChild(newBook);
 	}
 }
 
 function addBookToLibrary(title, author, pages) {
-	let id = `book${myLibrary.length}`;
-	nBookToCollection = new Book(title, author, pages, id);
+	const id = `book${myLibrary.length}`;
+	const nBookToCollection = new Book(title, author, pages, id);
 	myLibrary.push(nBookToCollection);
 	displayBooks(myLibrary);
 }
@@ -42,15 +45,14 @@ submitBtn.addEventListener("click", function (e) {
 	e.preventDefault();
 });
 
-let allBooks = bookShelf.querySelectorAll(".book-card");
+const allBooks = bookShelf.querySelectorAll(".book-card");
 allBooks.forEach((book) => {
 	book.addEventListener("click", function () {
-		let itSink = book.querySelector("div").id;
-		let r = myLibrary.findIndex((elemento) => {
-			return elemento.id == itSink;
+		const itSink = book.querySelector("div").id;
+		const r = myLibrary.findIndex((elemento) => {
+			return elemento.id === itSink;
 		});
 		myLibrary.splice(r, 1);
 		bookShelf.removeChild(book);
 	});
 });
-// is this a comment?
